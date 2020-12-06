f57eb55e59cfadf954da615c9ca33105c5268f5d
Ken Chan <kenchan0824@gmail.com>
2020-12-06 16:50:20 +0800

    Minor change.

--- The Skyline Problem/skyline.py
+++ The Skyline Problem/skyline.py
@@ -4,32 +4,31 @@ def eventscan(buildings):
 
     events = []
     for l, r, h in buildings:
-        events.append([l, -h])  # ensure incomming event goes first
-        events.append([r, h])   # outgoing event
-    events = sorted(events)     # order events by time
+        events.append([l, -h])                      # ensure incomming event goes first
+        events.append([r, h])                       # outgoing event
+    events = sorted(events)                         # order events by time
     
-    heights = []                # stacked heights
-    skyline = []                # results
-    max_height = 0              # handle corner cases
+    heights = []                                    # stacked heights
+    skyline = []                                    # results
+    max_height = 0                                  # handle corner cases
     
     for t, h in events:
 
-        if h < 0:               # incomming event
-            insort(heights, h)  # negative value, ensure max comes first
+        if h < 0:                                   # incomming event
+            insort(heights, h)                      # negative value, ensure max first
             if -h > max_height:
                 skyline.append([t, -h])
                 max_height = -h
-        else:                   # outgoing event
+        else:                                       # outgoing event
             heights.pop(bisect_left(heights, -h))
             curr_height = -heights[0] if heights else 0
-            if curr_height < max_height:        # handle corner cases
+            if curr_height < max_height:            # handle corner cases
                 max_height = curr_height
                 skyline.append([t, max_height])
 
     return skyline
 
-t, h = events[1]
-heights[0]
+
 if __name__ == '__main__':
     
     buildings = [[2,9,10],[3,7,15],[5,12,12],[15,20,10],[19,24,8]]
